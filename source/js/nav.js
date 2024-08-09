84b784c6eb10a4ccc96bcf39147770c07d4dc310
Margarita <balahninarita@gmail.com>
2024-08-09 22:19:18 +0300

    убирает меню при клике на ссылку

--- source/js/nav.js
+++ source/js/nav.js
@@ -1,5 +1,6 @@
 const mainNav = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-nav__toggle');
+const siteLinks = document.querySelectorAll('.site__link');
 
 const initOverlay = () => {
   document.body.classList.add('page-body--overlay');
@@ -9,20 +10,35 @@ const removeOverlay = () => {
   document.body.classList.remove('page-body--overlay');
 };
 
+const showNav = () => {
+  mainNav.classList.remove('main-nav--closed');
+  mainNav.classList.add('main-nav--opened');
+  initOverlay();
+};
+
+const hideNav = () => {
+  mainNav.classList.remove('main-nav--opened');
+  mainNav.classList.add('main-nav--closed');
+  removeOverlay();
+};
+
 const onNavToggleClick = () => {
   if (mainNav.classList.contains('main-nav--closed')) {
-    mainNav.classList.remove('main-nav--closed');
-    mainNav.classList.add('main-nav--opened');
-    initOverlay();
+    showNav();
   } else {
-    mainNav.classList.remove('main-nav--opened');
-    mainNav.classList.add('main-nav--closed');
-    removeOverlay();
+    hideNav();
   }
+};
+
+const onSiteLinkClick = () => {
+  hideNav();
 };
 
 const initNav = () => {
   navToggle.addEventListener('click', onNavToggleClick);
+  siteLinks?.forEach((link) => {
+    link.addEventListener('click', onSiteLinkClick);
+  });
 };
 
 export { initNav };
