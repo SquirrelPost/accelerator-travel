82e6645ee37245f2785f0f91ca00191be2824b85
Margarita <balahninarita@gmail.com>
2024-08-23 17:04:38 +0300

    стилизует планшет и десктоп туров

--- source/js/modules/card-slider.js
+++ source/js/modules/card-slider.js
@@ -5,10 +5,22 @@ import 'swiper/css';
 
 const cardSwiper = new Swiper('.card-swiper__slider', {
   loop: false,
+  slidesPerView: 1,
   modules: [ Navigation],
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    768: {
+      watchSlidesProgress: true,
+      slidesPerView: 2,
+      spaceBetween: 18,
+    },
+    1440: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
   },
 });
 
