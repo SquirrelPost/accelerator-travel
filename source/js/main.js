90b73c047845b3b8b0cb246a14adfd1ab79bd0f9
Margarita <balahninarita@gmail.com>
2024-08-30 13:19:52 +0300

    добавляет фотогалерею

--- source/js/main.js
+++ source/js/main.js
@@ -2,9 +2,11 @@ import { initNav } from './modules/nav.js';
 import './modules/hero-slider.js';
 import { createCardSwipers } from './modules/card-slider.js';
 import {createAdvSwiper} from './modules/adv-swiper.js';
+import {createPhotoSwiper} from './modules/photo-swiper.js';
 import {calcStarsWidth} from './modules/star-rating.js';
 
 initNav();
 calcStarsWidth();
 createCardSwipers();
 createAdvSwiper();
+createPhotoSwiper();
---
+++ source/js/modules/photo-swiper.js
@@ -0,0 +1,32 @@
+import Swiper from 'swiper';
+
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+
+const createPhotoSwiper = () => {
+  const photoSwiper = new Swiper('.photo-swiper__slider', {
+    enabled: true,
+    loop: true,
+    initialSlide: 0,
+    slidesPerView: 2,
+    spaceBetween: 6,
+    modules: [Navigation],
+    navigation: {
+      nextEl: '.photo-swiper__button--next',
+      prevEl: '.photo-swiper__button--prev',
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 5,
+      },
+      1440: {
+        slidesPerView: 5,
+        spaceBetween: 4,
+      },
+    },
+  });
+  photoSwiper.init();
+};
+
+export { createPhotoSwiper };
